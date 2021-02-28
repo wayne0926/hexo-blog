69a038c491a08b655572917761f0b872fc4b6b8f
时过境迁Wayne <3462757126@qq.com>
2021-02-28 13:29:43 +0800

    Update generate.py

--- generate.py
+++ generate.py
@@ -5,8 +5,8 @@ import requests
 sitemap = 'https://wr0926.ml/sitemap.xml'
 
 html = urllib.request.urlopen(sitemap).read().decode('utf-8')
-result = re.findall(re.compile(r'(?<=<loc>).*?(?=</loc>)'), html)
-
+result1 = html.replace('https://wr0926.ml', 'https://www.wr0926.ml')
+result = re.findall(re.compile(r'(?<=<loc>).*?(?=</loc>)'), result1)
 with open('urls.txt', 'w') as file:
   for data in result:
     print(data, file=file)
