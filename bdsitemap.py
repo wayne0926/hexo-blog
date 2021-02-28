dc74f21d3558fde60dcdbb54010c9fe5a92d61b1
时过境迁Wayne <3462757126@qq.com>
2021-02-28 14:02:36 +0800

    Create bdsitemap.py

---
+++ bdsitemap.py
@@ -0,0 +1,11 @@
+import re
+import urllib
+import requests
+import codecs
+
+sitemap = 'https://wr0926.ml/sitemap.xml'
+html = urllib.request.urlopen(sitemap).read().decode('utf-8')
+result = html.replace('https://wr0926.ml', 'https://www.wr0926.ml')
+f = codecs.open('baidu-sitemap.xml', mode='a', encoding='utf-8')
+f.write(result)
+f.close
