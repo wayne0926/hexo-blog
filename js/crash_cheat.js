4367903d11b332b43e9058c6783164105b82e4cf
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-09-15 12:22:49 +0000

    Deploy wayne0926/hexo-blog to github.com/wayne0926/hexo-blog.git:gh-pages

--- js/crash_cheat.js
+++ js/crash_cheat.js
@@ -1,1 +1,1 @@
-var OriginTitle = document.title; var titleTime; document.addEventListener("visibilitychange", function () { if (document.hidden) { } else { $('[rel="icon"]').attr("href", "/favicon.ico"); document.title = "(≧▽≦q)可爱的你回来了"; titleTime = setTimeout(function () { document.title = OriginTitle }, 1300) } });
+var titleTime,OriginTitle=document.title;document.addEventListener("visibilitychange",function(){document.hidden||($('[rel="icon"]').attr("href","/favicon.ico"),document.title="(≧▽≦q)可爱的你回来了",titleTime=setTimeout(function(){document.title=OriginTitle},1300))});
